394193989bd90ebe1817be919c86aba9624086d8
george <george.6809@gmail.com>
2022-06-08 20:57:17 +0900

    first commit.

---
+++ init.lisp
@@ -0,0 +1,13 @@
+(defpackage #:lem-my-init
+  (:use #:cl
+        #:lem))
+(in-package :lem-my-init)
+
+(define-key *global-keymap* "Return" 'lem.language-mode:newline-and-indent)
+(setf *scroll-recenter-p* nil)
+
+(let ((asdf:*central-registry* (cons #P"~/.lem/" asdf:*central-registry*)))
+  (ql:quickload :lem-my-init))
+(define-command slime-qlot-exec (directory) ((list (prompt-for-directory "Project directory: " (buffer-directory))))
+  (change-directory directory)
+  (lem-lisp-mode:run-slime (lem-lisp-mode::get-lisp-command :prefix "qlot exec ")))
